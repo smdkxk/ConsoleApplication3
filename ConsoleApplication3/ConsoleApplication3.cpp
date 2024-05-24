4b5ad2186fa25e2f8b6aa5d090365507524d869d
Yaroslav <nevidovv22@gmail.com>
2024-05-25 01:20:04 +0800

    добавил некоторые изменения

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -3,11 +3,16 @@
 
 int main() {
     std::string name;
+    int age;
 
     std::cout << "Введите ваше имя: ";
     std::getline(std::cin, name);
 
+    std::cout << "Введите ваш возраст: ";
+    std::cin >> age;
+
     std::cout << "Привет, " << name << "!" << std::endl;
+    std::cout << "Вам " << age << " лет." << std::endl;
 
     return 0;
 }
