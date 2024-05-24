c604f44747072abf64cc903d2a9d3c94a56d4cfc
Yaroslav <nevidovv22@gmail.com>
2024-05-25 01:18:31 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+
+    std::cout << "Введите ваше имя: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Привет, " << name << "!" << std::endl;
+
+    return 0;
+}
